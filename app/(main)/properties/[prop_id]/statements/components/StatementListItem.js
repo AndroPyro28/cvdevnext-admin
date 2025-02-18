6656a2b0a7b5772784b49f217f0cce2b1803cbe0
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-02-18 15:47:51 +0800

    save

--- app/(main)/properties/[prop_id]/statements/components/StatementListItem.js
+++ app/(main)/properties/[prop_id]/statements/components/StatementListItem.js
@@ -7,12 +7,14 @@ import statements from "../statements.module.css";
 
 export default function StatementListItem(props) {
     const { propId, statement } = props;
-    const { bll_bill_cov_period, bll_id, bll_pay_stat } = statement;
+    const { bll_bill_cov_period, bll_id, bll_pay_stat, transactions_status } = statement;
 
+    const status = bll_pay_stat === "paid" && transactions_status === "completed" ? "Paid" : "Pending"
     // Format the bll_bill_cov_period using Luxon
     const formattedPeriod = DateTime.fromFormat(bll_bill_cov_period, "yyyy-MM")
         .toFormat("MMMM yyyy");
 
+        console.log(props)
     return (
         <div className={statements.statements_list_item_div}>
             <div className={statements.statements_list_item_date_div}>
@@ -22,7 +24,7 @@ export default function StatementListItem(props) {
                 <p className={statements.statements_list_item_id}>{bll_id}</p>
             </div>
             <div className={statements.statements_list_item_status_div}>
-                <p className={statements.statements_list_item_status}>PAID</p>
+                <p className={statements.statements_list_item_status}>{status}</p>
             </div>
             <div className={statements.statements_list_item_cta_div}>
                 <Link className={statements.statements_list_item_cta} href={`/properties/${propId}/statements/${bll_id}`}>View Info</Link>
